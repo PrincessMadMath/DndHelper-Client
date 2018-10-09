7421b017f948b73ed0dd9bc10b17f0e3432318e3
Konstantin <fedorov.konstantin@gmail.com>
2018-10-08 19:06:28 -0700

    Adds sorting on monster CR

--- spa/src/Components/MonsterDatabase.js
+++ spa/src/Components/MonsterDatabase.js
@@ -1,15 +1,25 @@
 import React from "react";
 import MonsterList from "./MonsterList";
-import MonsterFilter from "./MonsterFilter";
+import SearchBox from "./SubComponents/SearchBox";
+import MultiSelect from "./SubComponents/MultiSelect";
 
 class MonsterDatabase extends React.Component {
-    state = {
-        filter: null,
-        encounterMonster: [],
-    };
+    constructor(props) {
+        super(props);
 
-    handleFilterUpdate = filter => {
-        this.setState({ filter: filter });
+        this.state = {
+            masks: {
+                name_mask: new Array(props.monstersDB.length).fill(true),
+                cr_mask: new Array(props.monstersDB.length).fill(true)
+            },
+            encounterMonster: [],
+        };
+    }
+
+    setMask = (mask, mask_name) => {
+        // we have to force a lazy-load check after filtering children since spells
+        // can enter the viewport without scroll or resize
+        this.setState({ masks: { ...this.state.masks, [mask_name]: mask } });
     };
 
     handleAddMonster = monsterName => {
@@ -26,34 +36,26 @@ class MonsterDatabase extends React.Component {
         this.props.history.push(`/encounter/?list=${this.state.encounterMonster.join()}`);
     };
 
-    selectVisibleMonster = () => {
-        const { monstersDB } = this.props;
-        const { filter } = this.state;
-
-        if (!!!filter) {
-            return monstersDB;
-        }
-
-        const visibleMonsters = monstersDB.filter(monster => {
-            const nameFilter = filter.name;
-            return monster.name.toLowerCase().includes(nameFilter.toLowerCase());
-        });
-
-        return visibleMonsters;
+    filterMonster = (monster, index) => {
+        return Object.values(this.state.masks).every(mask => mask[index]);
     };
 
     render() {
-        const { spellsDB } = this.props;
-
-        const visibleMonsters = this.selectVisibleMonster();
+        const { spellsDB, monstersDB } = this.props;
 
         return (
             <div>
                 <h3>Monster Database</h3>
                 <button onClick={this.handleGoToEncounter}>Go to encounter</button>
-                <MonsterFilter onFilterUpdate={this.handleFilterUpdate} />
+                <SearchBox
+                    fieldName={"name"}
+                    callback={mask => this.setMask(mask, "name_mask")}
+                    items={monstersDB.map(s => s.name)}
+                />
+                <MultiSelect items={monstersDB.map(m => m.challengeRating)}
+                             callback={mask => this.setMask(mask, "cr_mask")} />
                 <MonsterList
-                    visibleMonsters={visibleMonsters}
+                    visibleMonsters={monstersDB.filter(this.filterMonster)}
                     spellsDB={spellsDB}
                     onAddMonster={this.handleAddMonster}
                 />
