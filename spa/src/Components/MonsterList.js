7836bbbf1573aafac428ce3506d70dc303888055
Konstantin <fedorov.konstantin@gmail.com>
2018-10-10 21:23:13 -0700

    Do not unmount items in DBs, it actually breaks stuff

--- spa/src/Components/MonsterList.js
+++ spa/src/Components/MonsterList.js
@@ -31,7 +31,7 @@ export default class MonsterList extends Component {
             <div>
                 {visibleMonsters.map(function(monster) {
                     return (
-                        <LazyLoad key={monster.name} height={88} unmountIfInvisible={true}>
+                        <LazyLoad key={monster.name} height={88} once={true}>
                             <Monster monster={monster} opened={visibleMonsters.length === 1}>
                                 <AddButton>
                                     <button onClick={() => onAddMonster(monster.name)}>+</button>
