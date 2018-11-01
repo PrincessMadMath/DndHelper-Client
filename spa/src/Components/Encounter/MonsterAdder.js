97b1db9b8c6bf9b01171e606e262798823415533
Mathieu Gamache <mathg99@hotmail.com>
2018-10-31 22:05:54 -0400

    Quick thing to add monster in the encounter builder

---
+++ spa/src/Components/Encounter/MonsterAdder.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Select from "react-select";
+import { DBContext } from "../App";
+
+const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+];
+
+export default class MonsterAdder extends Component {
+    static propTypes = {
+        onAddMonster: PropTypes.func.isRequired,
+    };
+
+    state = {
+        selectedOption: null,
+    };
+
+    getMonsterOption = monstersDB => {
+        return monstersDB.map(x => ({
+            label: x.name,
+            value: x.name,
+            info: x,
+        }));
+    };
+
+    onAddMonster = () => {
+        const { info: monsterInfo } = this.state.selectedOption;
+        if (!monsterInfo) {
+            return;
+        }
+
+        this.props.onAddMonster(monsterInfo);
+        this.setState({
+            selectedOption: null,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <DBContext.Consumer>
+                    {value => (
+                        <Select
+                            value={this.state.selectedOption}
+                            onChange={selectedOption => this.setState({ selectedOption })}
+                            options={this.getMonsterOption(value.monstersDB)}
+                        />
+                    )}
+                </DBContext.Consumer>
+
+                <button onClick={this.onAddMonster}>Add monster</button>
+            </div>
+        );
+    }
+}
